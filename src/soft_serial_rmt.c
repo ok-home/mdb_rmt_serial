3d1f9f67f6b1cd17320c163fb9462fdc5b313d83
ok-home <ok-home@ya.ru>
2024-03-12 21:45:25 +0700

    tested decoder -> bad cod

--- src/soft_serial_rmt.c
+++ src/soft_serial_rmt.c
@@ -23,11 +23,14 @@ static const char *TAG = "SoftSerialRmt";
 #define RMT_DIV 8
 #define RMT_RX_IDLE_THRES 12000
 #define RX_BIT_DIVIDER 1040
-#define BIT_IN_WORD (10)  // start+8bit+stop 
+#define BIT_IN_WORD (10) // start+8bit+stop
 // single rmt item
-typedef struct {
-    union {
-        struct {
+typedef struct
+{
+    union
+    {
+        struct
+        {
             uint16_t duration : 15; /*!< Duration of level */
             uint16_t level : 1;     /*!< Level  */
         };
@@ -59,15 +62,15 @@ esp_err_t soft_serial_write_data(uint8_t *data, size_t count)
     return ESP_OK;
 }
 
-esp_err_t soft_serial_read_data(uint8_t *data, size_t count)
+esp_err_t soft_serial_read_data(uint8_t *data_out, size_t count)
 {
     size_t length = 0;
     // bool repeat = false;
     RingbufHandle_t rb = NULL;
     rmt_item16_t *items = NULL;
     uint8_t data[64] = {0};
-    int cnt_bit=0; // wait start bit
-    int cnt_byte=0;  // byte in packet
+    int cnt_bit = 0;  // wait start bit
+    int cnt_byte = 0; // byte in packet
     rmt_get_ringbuf_handle(RX_CHANNEL, &rb);
     rmt_rx_start(RX_CHANNEL, true);
     while (1)
@@ -78,42 +81,50 @@ esp_err_t soft_serial_read_data(uint8_t *data, size_t count)
             length /= 2; // one RMT = 2 Bytes
             for (int i = 0; i < length; i++)
             {
-                int cnt_in_duration = items[i].duration/RX_BIT_DIVIDER;
+                int cnt_in_duration = items[i].duration / RX_BIT_DIVIDER;
                 int lvl = items[i].level;
-                ESP_LOGI(TAG, "%d lvl=%d, bit_in=%d,dur=%d",i, level,cnt_in_duration,items[i].duration);
-                if(cnt_bit == 0){
+                //ESP_LOGI(TAG, "%d lvl=%d, bit_in=%d,dur=%d", i, lvl, cnt_in_duration, items[i].duration);
+                if (cnt_bit == 0)
+                {
                     if (lvl == 0 && cnt_in_duration > 0 && cnt_in_duration < BIT_IN_WORD) // start bit
                     {
-                        data[cnt_byte] = 0; // first  bits in byte
+                        data[cnt_byte] = 0;        // first  bits in byte
                         cnt_bit = cnt_in_duration; // start bit + some bits=0
                     }
                     else
                     {
-                        ESP_LOGE(TAG,"receive frame err START bit");
+                        ESP_LOGE(TAG, "receive frame err START bit");
                     }
-                } 
-                else if(cnt_in_duration == 0) // last item -> stop bit -> lvl=1
-                {
-                    lvl=1; //??
-                    for(;cnt_bit<BIT_IN_WORD-1;cnt_bit++)
-                    {
-                        data[cnt_byte] |= lvl<<7; // 8 with cmd or parity check //BIT_IN_WORD-3
-                        data[cnt_byte] >>= 1;
-                    }
-                    ESP_LOGI(TAG,"last item cnt %d data %0x %c",cnt_byte,data[cnt_byte],data[cnt_byte]);
-                        cnt_byte = 0;
-                        cnt_bit = 0; // wait next start bit
                 }
-                else 
+                else if (cnt_in_duration == 0) // last item -> stop bit -> lvl=1
                 {
-                    for(int j=0;(cnt_bit<BIT_IN_WORD-1) && (j < cnt_in_duration) ;cnt_bit++,j++)
+                    lvl = 1; //??
+                    for (; cnt_bit < BIT_IN_WORD - 1; cnt_bit++)
                     {
-                        data[cnt_byte] |= lvl<<7; // 8 with cmd or parity check //BIT_IN_WORD-3
                         data[cnt_byte] >>= 1;
+                        data[cnt_byte] |= lvl << 7; // 8 with cmd or parity check //BIT_IN_WORD-3
                     }
-                    if(cnt_bit >= BIT_IN_WORD-1) // all bits converted, next byte
+                    ESP_LOGI(TAG, "last item cnt %d data %0x ", cnt_byte, data[cnt_byte]);
+                    cnt_byte = 0;
+                    cnt_bit = 0; // wait next start bit
+                }
+                else if (cnt_bit >= BIT_IN_WORD - 1) // all bits converted, next byte
                     {
-                        ESP_LOGI(TAG,"last bit in byte cnt %d data %0x %c",cnt_byte,data[cnt_byte],data[cnt_byte]);
+                        ESP_LOGI(TAG, "last bit in byte cnt %d data %0x ", cnt_byte, data[cnt_byte]);
+                        cnt_byte++;
+                        cnt_bit = 0; // wait next start bit
+                    }
+
+                else
+                {
+                    int j=0;
+                    for (; (cnt_bit < BIT_IN_WORD - 1) && (j < cnt_in_duration); cnt_bit++, j++)
+                    {
+                        data[cnt_byte] >>= 1;
+                        data[cnt_byte] |= lvl << 7; // 8 with cmd or parity check //BIT_IN_WORD-3
+                    }
+                    if (j!=cnt_in_duration){
+                    ESP_LOGI(TAG, "double stop cnt=%d data=%x ", cnt_byte,data[cnt_byte]);
                         cnt_byte++;
                         cnt_bit = 0; // wait next start bit
                     }
@@ -121,9 +132,9 @@ esp_err_t soft_serial_read_data(uint8_t *data, size_t count)
             }
         }
         // after parsing the data, return spaces to ringbuffer.
-        ESP_LOGI(TAG,"all item converted");
-                        cnt_byte = 0;
-                        cnt_bit = 0; // wait next start bit
+        ESP_LOGI(TAG, "all item converted");
+        cnt_byte = 0;
+        cnt_bit = 0; // wait next start bit
         vRingbufferReturnItem(rb, (void *)items);
     }
 
